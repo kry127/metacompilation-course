f34c0d1f69cd303c41ce0017ba413f456c4a04f5
kry127 <kry127@yandex.ru>
2020-10-20 05:46:58 +0300

    Added mixmixmix mix = id mix, which performs perfectly

--- src/FC mix/program_examples.rkt
+++ src/FC mix/program_examples.rkt
@@ -7,41 +7,62 @@
   
   (provide find_name assign_name tm-example tm-left-expander)
 
-; Flow Chart programs
+  ;; Turing programs
 
-; 1. find 'name' in 'namelist' and give out corresponding value from 'valuelist'
-(define find_name
-  '((read name namelist valuelist)
-   (search (if (equal? name (car namelist)) found cont))
-   (cont (:= valuelist (cdr valuelist))
-         (:= namelist (cdr namelist))
-         (goto search))
-   (found (return (car valuelist)))
-   ))
+  ; 1. Find first '0' charactrer and change it to '1'
+  (define tm-example '((0 if 0 goto 3) (1 right) (2 goto 0) (3 write 1)))
 
-; 2. assign 'value' to variable named 'name'
-(define assign_name
-  '((read name value namelist valuelist)
-   (init (:= valuelist_left '())
-         (goto search))
-   (search (if (equal? name (car namelist)) found cont))
-   (cont (:= valuelist_left (cons (car valuelist) valuelist_left))
-         (:= valuelist (cdr valuelist))
-         (:= namelist (cdr namelist))
-         (goto search))
-   (found (:= valuelist_left (cons value valuelist_left))
-          (:= valuelist (cdr valuelist))
-          (return (append (reverse valuelist_left) valuelist)))
-   )
-  )
+  ; 2. Go to left several times and write some data
+  (define tm-left-expander '((0 left) (1 left) (2 left) (3 write 1) (4 left) (5 left) (6 write "#")))
 
-; Turing programs
+  ;; Flow Chart programs
 
-; 1. Find first '0' charactrer and change it to '1'
-(define tm-example '((0 if 0 goto 3) (1 right) (2 goto 0) (3 write 1)))
+  ; 1. find 'name' in 'namelist' and give out corresponding value from 'valuelist'
+  (define find_name
+    '((read name namelist valuelist)
+     (search (if (equal? name (car namelist)) found cont))
+     (cont (:= valuelist (cdr valuelist))
+           (:= namelist (cdr namelist))
+           (goto search))
+     (found (return (car valuelist)))
+     ))
 
-; 2. Go to left several times and write some data
-(define tm-left-expander '((0 left) (1 left) (2 left) (3 write 1) (4 left) (5 left) (6 write "#")))
+  ; 2. assign 'value' to variable named 'name'
+  (define assign_name
+    '((read name value namelist valuelist)
+     (init (:= valuelist_left '())
+           (goto search))
+     (search (if (equal? name (car namelist)) found cont))
+     (cont (:= valuelist_left (cons (car valuelist) valuelist_left))
+           (:= valuelist (cdr valuelist))
+           (:= namelist (cdr namelist))
+           (goto search))
+     (found (:= valuelist_left (cons value valuelist_left))
+            (:= valuelist (cdr valuelist))
+            (return (append (reverse valuelist_left) valuelist)))
+     )
+    )
+  ; 3. Flow Chart interpreter (self-interpreter)
+  (define int-fc-fc
+    '((read program valuelist)
+      (init (:= namelist (car program))
+            (:= program (cdr program))
+            (goto assign-loop))
 
+      ; find-name program
+      ;   input: name -- name of the key
+      ;   output: value -- corresponding value
+      (find-name
+            (:= find-name-namelist namelist)
+            (:= find-name-valuelist valuelist)
+            (goto find-name-search))
+      (find-name-search (if (equal? name (car find-name-namelist)) find-name-found find-name-cont))
+      (find-name-cont (:= find-name-valuelist (cdr find-name-valuelist))
+            (:= find-name-namelist (cdr find-name-namelist))
+            (goto find-name-search))
+      (find-name-found (return (car valuelist)))
+      ; find-name program
+      ))
+    
 
 )
--- src/FC mix/projection_3.rkt
+++ src/FC mix/projection_3.rkt
@@ -7,6 +7,8 @@
 (require "program_examples.rkt")
 
 ; Third Futamura projection
+;
+;   "Mix, mix, mix, mix, everybody starts to mix! Like dee dee dee, bah bah bah dah! Now you mixing with the mix."
 (define mixmixmix (flow-chart-int (flow-chart-mix 'env+) `(,(flow-chart-mix 'env-aux) ; program to specialize
                                                               (
                                                                (program division pp0 pending-lables pending-lables-iter pp bb
@@ -29,15 +31,13 @@
                                                                ) ; initial values of static variables
                                                               )))
 
-(println "Sing, sing, sing, sing!")
 ; pretty print program (returns pair of mapping of real labels to new labels + relabeled program itself)
-(println "III mapping")
-(car (flow-chart-pretty-printer mixmixmix))
+;(println "III mapping")
+;(car (flow-chart-pretty-printer mixmixmix))
 (println "III program")
 (cadr (flow-chart-pretty-printer mixmixmix))
 
-(println "Everybody starts to sing")
-; first Futamura projection with mixmixmix
+; generate compiler of turing languages by feeding interpreter in mixmixmix
 ; launch flow-chart-mix on Turing Machine interpreter
 (define mixmixmixed-TM-compiler (flow-chart-int mixmixmix `((,turing_machine ; = program
                                                                (
@@ -48,13 +48,51 @@
                                                                ) ; initial values of static variables
                                                               )))
 ; print out program
-(println "III compiler mapping")
-(car (flow-chart-pretty-printer mixmixmixed-TM-compiler))
+;(println "III compiler mapping")
+;(car (flow-chart-pretty-printer mixmixmixed-TM-compiler))
 (println "III compiler program")
 (cadr (flow-chart-pretty-printer mixmixmixed-TM-compiler))
-(println "When the body goes around")
 ; try to execute partially specialized program -- we'll get a compiled code :)
 (define mixmixmix-compiled-TM-example (flow-chart-int mixmixmixed-TM-compiler `((,tm-example ,tm-example () () () ()))))
 mixmixmix-compiled-TM-example
 (flow-chart-int mixmixmix-compiled-TM-example '((1 1 3 5 2 4 0)))
 
+
+; generate mix by feeding mix in mixmixmix (we need to go deeper)
+(define mixmixmix-mix (flow-chart-int mixmixmix `((,(flow-chart-mix '++env) ; program to specialize
+                                                              (
+                                                               (program division pp0 pending-lables pending-lables-iter pp bb
+                                                                        command X expr
+                                                                        predicate pp-true pp-false next-label
+                                                                        header) ; static
+                                                               (ppd pending marked residual ++env code vs spec-state
+                                                                          label-true label-false) ; dynamic
+                                                               ) ; division
+                                                              () () () () () () () () () () () () ()
+                                                              ))))
+; we've got mix itself!!!
+; pretty print program (returns pair of mapping of real labels to new labels + relabeled program itself)
+;(println "III mixmixmix-mix (= id mix) mapping")
+;(car (flow-chart-pretty-printer mixmixmix-mix))
+(println "III mixmixmix-mix (= id mix) program")
+(cadr (flow-chart-pretty-printer mixmixmix-mix))
+; check that mix specializes 'tm-example' program from turing language to flow-chart language
+(define mixmixmix-mixed-FC-TM (flow-chart-int mixmixmix-mix `(
+                                                              (,turing_machine ; = program
+                                                               (
+                                                                (program prog instruction instruction-operator expr step) ; static
+                                                                (left right element) ; dynamic)
+                                                                ) ; = division
+                                                               () () () () () () () () () () () () ()
+                                                               ) ; initial values of static variables
+                                                              )))
+
+;(println "III mixmixmix-mixed-FC-TM mapping")
+;(car (flow-chart-pretty-printer mixmixmix-mixed-FC-TM))
+(println "III mixmixmix-mixed-FC-TM program")
+(cadr (flow-chart-pretty-printer mixmixmix-mixed-FC-TM))
+; compile program with this insanity :/
+(define mixmixmix-mixed-compiled-tm-example (flow-chart-int mixmixmix-mixed-FC-TM `((,tm-example ,tm-example () () () ()))))
+; check that compiled program makes what it does
+(flow-chart-int mixmixmix-mixed-compiled-tm-example '((7 -6 8 0 915 -5 0 0 53)))
+; expected: '(7 -6 8 1 915 -5 0 0 53)
